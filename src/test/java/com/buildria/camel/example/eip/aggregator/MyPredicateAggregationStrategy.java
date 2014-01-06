d4f714434f7a799e0ea1499f82110f6b6d43b878
Seiji Sogabe <s.sogabe@gmail.com>
2014-01-06 10:05:59 +0900

    Aggregator completionPredicate

--- src/test/java/com/buildria/camel/example/eip/aggregator/MyPredicateAggregationStrategy.java
+++ src/test/java/com/buildria/camel/example/eip/aggregator/MyPredicateAggregationStrategy.java
@@ -3,12 +3,14 @@ package com.buildria.camel.example.eip.aggregator;
 import org.apache.camel.Exchange;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
 
-public class MyAggregationStrategy implements AggregationStrategy {
+public class MyPredicateAggregationStrategy implements AggregationStrategy {
 
     /**
      * <p>メッセージを集約します。</p>
      * 
      * 取得したString型のメッセージを<code>List&lt;String&gt;</code>に追加します。
+     * 取得したString型のメッセージに"END"が含まれている場合、
+     * ヘッダ<code>completion</code>に<code>true</code>を設定します。
      * 
      * <code>newExchange</code>は、これから集約するExchange、
      * <code>oldExchange</code>は、集約識別単位で集約中のExchangeです。
@@ -32,6 +34,10 @@ public class MyAggregationStrategy implements AggregationStrategy {
         String oldBody = oldExchange.getIn().getBody(String.class);
         // 新規に集約する文字列を習得
         String newBody = newExchange.getIn().getBody(String.class);
+        // "END"を含む場合、ヘッダに完了条件を設定
+        if (newBody != null && newBody.contains("No.4")) {
+            oldExchange.getIn().setHeader("completion", true);
+        }
         // CSV形式の文字列を生成して再設定
         oldExchange.getIn().setBody(oldBody + "," + newBody);
 
