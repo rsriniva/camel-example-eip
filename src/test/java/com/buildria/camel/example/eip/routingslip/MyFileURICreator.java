9c20e3a24546ac3ae6277cf180117e6058d96662
ssogabe <s.sogabe@gmail.com>
2014-01-07 08:36:15 +0900

    use <method< insted of <processor>

---
+++ src/test/java/com/buildria/camel/example/eip/routingslip/MyFileURICreator.java
@@ -0,0 +1,13 @@
+package com.buildria.camel.example.eip.routingslip;
+
+import org.apache.camel.Exchange;
+
+public class MyFileURICreator {
+
+    public String create(Exchange exchange) throws Exception {
+        // ヘッダ"pref"の値を取得
+        String pref = exchange.getIn().getHeader("pref", String.class);
+        // FileプロデューサのURIを生成
+       return "file:work/" + pref;
+    }
+}
