0dc8a452c8c3fa9b9b26997c153797ca9282c23b
ssogabe <s.sogabe@gmail.com>
2014-01-30 06:29:52 +0900

    Added comment

--- src/test/java/com/buildria/camel/example/eip/cbr/HeaderChecker.java
+++ src/test/java/com/buildria/camel/example/eip/cbr/HeaderChecker.java
@@ -4,6 +4,12 @@ import org.apache.camel.Exchange;
 
 public class HeaderChecker {
 
+    /**
+     * ヘッダ"type"の値が"A"であるかチェックします。
+     * 
+     * @param exchange Exchange
+     * @return  ヘッダ"type"の値が"A"の場合<code>true</code>
+     */
     public boolean isTypeA(Exchange exchange) {
         String type = exchange.getIn().getHeader("type", String.class);
         return "A".equalsIgnoreCase(type);
