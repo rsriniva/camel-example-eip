88eff358c39da626b0aef31e8f9d0991068ed7c1
ssogabe <s.sogabe@gmail.com>
2014-02-02 06:07:53 +0900

    added property file

---
+++ src/test/java/com/buildria/camel/example/basis/property/PropertyCamelClassPathTest.java
@@ -0,0 +1,37 @@
+package com.buildria.camel.example.basis.property;
+
+import com.buildria.camel.example.JunitBase;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class PropertyCamelClassPathTest extends JunitBase {
+
+    @Autowired
+    protected CamelContext camelContext;
+
+    private ProducerTemplate template;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        template = camelContext.createProducerTemplate();
+    }
+
+    @Test
+    @DirtiesContext
+    public void testClassPath() throws Exception {
+        template.sendBody("direct:camelpath", "sample data");
+    }
+
+
+}
